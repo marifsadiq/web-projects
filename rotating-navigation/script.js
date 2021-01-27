cabfcc26fefac8abab4ec9b9cf2994f306dd9916
marifsadiq <marifsadiq@gmail.com>
2021-01-27 21:16:52 +0500

    Fixing Errors

--- rotating-navigation/script.js
+++ rotating-navigation/script.js
@@ -5,9 +5,11 @@ const circle = document.getElementById('circle')
 
 openBtn.addEventListener('click', () => {
   container.classList.add('show-nav')
+  document.body.style.overflowY = 'hidden'
   circle.style.transform = 'rotate(-90deg)'
 })
 closeBtn.addEventListener('click', () => {
   container.classList.remove('show-nav')
+  document.body.style.overflowY = 'auto'
   circle.style.transform = 'rotate(0deg)'
 })
